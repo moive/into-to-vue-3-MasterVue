ada11eff497ca313724397f2da4113d03b4bdb81
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-10-12 22:46:18 -0500

    form model

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -48,6 +48,8 @@ app.component('product-display', {
                 </button>
             </div>
         </div>
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+        <review-form @review-submitted="addReview"></review-form>
     </div>`,
     data(){
         return {
@@ -62,6 +64,7 @@ app.component('product-display', {
                 {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:0},
             ],
             cart: 0,
+            reviews: []
         }
     },
     computed: {
@@ -92,6 +95,9 @@ app.component('product-display', {
             this.selectedVariant = index;
             this.onSale = this.variants[this.selectedVariant].quantity > 10;
             console.log(index)
+        },
+        addReview(review){
+            this.reviews.push(review);
         }
     }
 })
