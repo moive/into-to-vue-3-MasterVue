1b3943651abf0db3ee503470485bc45486438779
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-10-12 20:01:57 -0500

    Computed properties

--- main.js
+++ main.js
@@ -2,16 +2,30 @@ const app = Vue.createApp({
     data(){
         return {
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
+            brand: "Moive",
+            selectedVariant: 0,
             inventory: 8,
             onSale: true,
             details: ['50% cotton','30% wool', '20% polyester'],
             variants: [
-                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'},
+                {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity:50},
+                {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:0},
             ],
             cart: 0,
-            inStock: false,
+        }
+    },
+    computed: {
+        title(){
+            return `${this.brand} ${this.product}`
+        },
+        image(){
+            return this.variants[this.selectedVariant].image;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity;
+        },
+        textOnSale(){
+            return `${this.title} is on sale`
         }
     },
     methods: {
@@ -22,9 +36,10 @@ const app = Vue.createApp({
             if(this.cart == 0) return;
             this.cart -= 1;
         },
-        UpdateImage(image){
-            if(image == null) return;
-            this.image = image;
+        UpdateVariant(index){
+            this.selectedVariant = index;
+            this.onSale = this.variants[this.selectedVariant].quantity > 10;
+            console.log(index)
         }
     }
 });
